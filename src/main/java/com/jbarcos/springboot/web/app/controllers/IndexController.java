8c2cbfb9fddf0325f9fceb583928f9ecd84356c3
jorgebarcos <jorgebarcos88@gmail.com>
2022-08-20 09:52:34 -0300

    el objeto model

--- src/main/java/com/jbarcos/springboot/web/app/controllers/IndexController.java
+++ src/main/java/com/jbarcos/springboot/web/app/controllers/IndexController.java
@@ -1,9 +1,11 @@
 package com.jbarcos.springboot.web.app.controllers;
 
+import com.jbarcos.springboot.web.app.models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
@@ -11,12 +13,26 @@ import java.util.Objects;
 
 
 @Controller
+@RequestMapping("/app")
 public class IndexController {
 
     @GetMapping({"/index", "/", "/home"})
      public String index(Model model) {
         model.addAttribute("titulo", "Hola Spring Framework con Model");
         return "index";
+    }
+
+    @RequestMapping("/perfil")
+    public String perfil(Model model) {
+
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Jorge");
+        usuario.setApellido("Barcos");
+
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+
+        return "perfil";
     }
 
     /******OTRAS FORMAS DE RETORNAR DATOS A LA VISTA******/
